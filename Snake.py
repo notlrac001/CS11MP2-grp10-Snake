546be1aaae5f1f4eba00947084bb9f8fa1a55d69
notlrac001 <castu65258@gapps.uwcsea.edu.sg>
2018-11-20 19:32:35 +0800

    Add files via upload
    Updated snake class to integrate it with Pyglet.

--- Snake.py
+++ Snake.py
@@ -1,30 +1,57 @@
+import pyglet
+import Segment as Seg
+
 class Snake:
 	''' Snake class represents the snake for the game. Contains essential information
 		about the snake such as position information, name of player (useful for 
 		multiplayer/saving names of highscorers)
 		
-		Attributes: 
-		playerName:str
-		position:tuple
-		length:int
-		squaresoccupied:list of tuples
-		score:int
-		direction:int (1=up, -1=down, 2=right, -2=left)
+		Attributes:
+		type (1=player1, 2=player2)
+		playerName
+		positionX
+		positionY
+		length
+		squaresoccupied
+		score
+		direction (1=up, -1=down, 2=right, -2=left)
+
+
+		pyglet related:
+		snakeSprite
+		segments
+		segBatch
 	'''
 
-	
+	def __init__(self,name,x,y,playerType):
 
-	def __init__(self,name,x,y):
+
+		head = pyglet.image.load('head.png')
+		head.width = 36
+		head.height = 36
+		head.anchor_x = 18
+		head.anchor_y = 18
+
+		self.playerType = playerType
 		self.playerName = name
-		self.length = 10
+		self.length = 7
 		self.direction = 1
 		self.positionX = x
 		self.positionY = y
 		self.squaresoccupied = list()
 		self.score = int()
+		self.moveQueue = list()
+
+		self.segments = list()
+		self.segBatch = pyglet.graphics.Batch()
+		self.snakeSprite = pyglet.sprite.Sprite(head)
 
 		for i in range(self.length):
 			self.squaresoccupied.append(tuple((x,y+i)))
+			self.segments.append(Seg.Segment(340+(x*40),60+(y*40)+(i*40),self.direction,self.playerType))
+
+		for element in self.segments:
+			element.segmentSprite.batch = self.segBatch
 
 	def setSquares(self):
 		'''
@@ -38,12 +65,35 @@ class Snake:
 		if len(self.squaresoccupied) > self.length:
 			self.squaresoccupied.pop()
 
+	def setSegments(self):
+		'''
+		See the documentation for setSquares(). It works similarly, except handles the segments
+		of the snake for drawing in pyglet. Also specifies the batches of the snake.
+		'''
+		self.segments.insert(0,Seg.Segment(340+(self.positionX*40),(60+(self.positionY*40)),self.direction,self.playerType))
+
+		if len(self.segments) > self.length:
+			self.segments.pop()
+
+		for element in self.segments:
+			element.segmentSprite.batch = self.segBatch
+
 	def setDirection(self,direction):
 		'''
 		Set the direction that the snake is facing. 
 		'''
+
 		if self.direction != -direction:
 			self.direction = direction
+			if self.direction == 1:
+				self.snakeSprite.rotation = 180
+			elif self.direction == -1:
+				self.snakeSprite.rotation = 0
+			elif self.direction == 2:
+				self.snakeSprite.rotation = 90
+			elif self.direction == -2:
+				self.snakeSprite.rotation = 270
+
 
 	def eat(self):
 		'''
@@ -71,6 +121,11 @@ class Snake:
 		if self.direction == -2:
 			self.setSquares()
 			self.positionX -= 1
+
+		#Mostly a debug function
+		print(str(self.positionX) + " " + str(self.positionY))
+
+
 
 
 		
